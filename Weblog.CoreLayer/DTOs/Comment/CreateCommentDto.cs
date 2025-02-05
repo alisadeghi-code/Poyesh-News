697c9da0d7d308b86a52dcca734f76e99cd4b6a5
Ali | علی <alisadeghicode@gmail.com>
2025-02-05 15:59:47 +0330

    Add-CommentDto

---
+++ Weblog.CoreLayer/DTOs/Comment/CreateCommentDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weblog.CoreLayer.DTOs.Comment
+{
+	public class CreateCommentDto
+	{
+		public int PostId { get; set; }
+
+		public int UserId { get; set; }
+
+		public string Text { get; set; }
+	}
+
+	public class CommentDto : CreateCommentDto 
+	{
+		public string UserFullName { get; set; }
+	}
+}
