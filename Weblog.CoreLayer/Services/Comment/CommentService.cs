017e048c3d946b58d05428c60c6bf3f6f850d508
Ali | علی <alisadeghicode@gmail.com>
2025-02-05 16:03:59 +0330

    Add-CommentDto-in-CommentService

--- Weblog.CoreLayer/Services/Comment/CommentService.cs
+++ Weblog.CoreLayer/Services/Comment/CommentService.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Weblog.CoreLayer.DTOs.Comment;
 
 namespace Weblog.CoreLayer.Services.Comment
 {
-	public class CommentService:ICommentService
+	public class CommentService : ICommentService
 	{
+		public void CreateComment(CreateCommentDto command)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<CommentDto> GetPostComments(int postId)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
--- Weblog.CoreLayer/Services/Comment/ICommentService.cs
+++ Weblog.CoreLayer/Services/Comment/ICommentService.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Weblog.CoreLayer.DTOs.Comment;
 
 namespace Weblog.CoreLayer.Services.Comment
 {
 	public interface ICommentService
 	{
+		void CreateComment(CreateCommentDto command);
+		List<CommentDto> GetPostComments(int postId);
 	}
 }
