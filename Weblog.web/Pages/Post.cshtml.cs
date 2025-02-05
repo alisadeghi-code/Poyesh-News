a9f50c8ea9c393598da078e84978caba995900d4
Ali | علی <alisadeghicode@gmail.com>
2025-02-05 16:48:34 +0330

    Add-Comment-in-PostModel

--- Weblog.web/Pages/Post.cshtml.cs
+++ Weblog.web/Pages/Post.cshtml.cs
@@ -1,20 +1,40 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Weblog.CoreLayer.DTOs.Post;
+using Weblog.CoreLayer.DTOs.Comment;
+using Weblog.CoreLayer.Services.Comment;
 using Weblog.CoreLayer.Services.Post;
+using Weblog.CoreLayer.Utilities;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 
 namespace Weblog.web.Pages
 {
     public class PostModel : PageModel
     {
         private readonly IPostService _postService;
+        private readonly ICommentService _commentService;
 
-        public PostModel(IPostService postService)
+		public PostModel(ICommentService commentService)
+		{
+			_commentService = commentService;
+		}
+
+		public PostModel(IPostService postService)
         {
             _postService = postService;
         }
 
         public PostDto Post { get; set; }
+        [BindProperty]
+        [Required(ErrorMessage ="متن خود را وارد کنید")]
+        public string Text { get; set; }
+        [BindProperty]
+        public int PostId { get; set; }
+
+
+        public List <CommentDto> Comments { get; set; }
         public IActionResult OnGet(string slug)
         {
             Post=_postService.GetPostBySlug(slug);
@@ -22,7 +42,28 @@ namespace Weblog.web.Pages
             {
                 return RedirectToAction("Error");
             }
+
+            Comments = _commentService.GetPostComments(Post.PostId);
             return Page();
         }
+
+        public IActionResult OnPost(string slug) 
+        {
+            if (!User.Identity.IsAuthenticated)
+                return RedirectToPage("Auth/SignIn");
+            if (!ModelState.IsValid) 
+            {
+                Post = _postService.GetPostBySlug(slug);
+                return Page();
+            }
+
+            _commentService.CreateComment(new CreateCommentDto()
+            {
+                UserId = User.GetUserId(),
+                PostId = PostId,
+                Text = Text,
+            });
+			return RedirectToPage("Post",new {slug});
+		}
     }
 }
